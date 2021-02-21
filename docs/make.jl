a6d8fad2056f74a0af8510ac2dd337dbd4f3daa2
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-02-21 02:33:02 +0100

    try to fix error messages

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,9 @@ using Documenter, PackageAnalyzer
 
 DocMeta.setdocmeta!(PackageAnalyzer, :DocTestSetup, :(using PackageAnalyzer); recursive=true)
 
+previous_GKSwstype = get(ENV, "GKSwstype", "")
+ENV["GKSwstype"] = "100"
+
 makedocs(
     format = Documenter.HTML(
         prettyurls = true
@@ -19,3 +22,6 @@ deploydocs(
     push_preview=true,
     devbranch = "main",
 )
+
+# restore the environmental variable `GKSwstype`.
+ENV["GKSwstype"] = previous_GKSwstype;
