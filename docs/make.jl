b96a6f673bacd47ca7c199ca5fb34d416f3ee686
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-02-21 03:32:37 +0100

    use `withenv`

--- docs/make.jl
+++ docs/make.jl
@@ -2,26 +2,23 @@ using Documenter, PackageAnalyzer
 
 DocMeta.setdocmeta!(PackageAnalyzer, :DocTestSetup, :(using PackageAnalyzer); recursive=true)
 
-previous_GKSwstype = get(ENV, "GKSwstype", "")
-ENV["GKSwstype"] = "100"
+withenv("GKSwstype" => "100") do
+    makedocs(
+        format = Documenter.HTML(
+            prettyurls = true
+        ),
+        modules = [PackageAnalyzer],
+        sitename = "PackageAnalyzer.jl",
+        pages = ["Home" => "index.md",
+                "API Reference" => "api.md",
+                "Saving results" => "serialization.md",
+                "A look at the General registry" => "look_at_general.md"]
+    )
 
-makedocs(
-    format = Documenter.HTML(
-        prettyurls = true
-    ),
-    modules = [PackageAnalyzer],
-    sitename = "PackageAnalyzer.jl",
-    pages = ["Home" => "index.md",
-             "API Reference" => "api.md",
-             "Saving results" => "serialization.md",
-             "A look at the General registry" => "look_at_general.md"]
-)
+    deploydocs(
+        repo = "github.com/JuliaEcosystem/PackageAnalyzer.jl.git",
+        push_preview=true,
+        devbranch = "main",
+    )
 
-deploydocs(
-    repo = "github.com/JuliaEcosystem/PackageAnalyzer.jl.git",
-    push_preview=true,
-    devbranch = "main",
-)
-
-# restore the environmental variable `GKSwstype`.
-ENV["GKSwstype"] = previous_GKSwstype;
+end
